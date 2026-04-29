f950fe15c753c614b30e71dd22beb98b102261f0
witchbaik <daffafariz31@gmail.com>
2026-04-29 12:04:13 +0700

    buat landing page dummy

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Membuat rute bernama 'login' yang otomatis melempar user ke halaman Filament
+Route::get('/login', function () {
+    return redirect('/admin/login');
+})->name('login');
